af47aba8e11b8fd9805186c60b5312553ed9c6b1
tycarroll <tycarroll@bethel.jw.org>
2024-07-01 12:30:14 -0400

    Modified default.rb recipe and added configuration and test files

--- appserv_kanboard_cb/recipes/default.rb
+++ appserv_kanboard_cb/recipes/default.rb
@@ -8,23 +8,93 @@ apt_update 'update_sources' do
   action :update
 end
 
-# Install Apache
-package 'apache2, 'php' do
+# Install Apache and PHP
+package ['apache2', 'php'] do
   action :install
 end
 
-# Install additional PHP packages
-package ['libapache2-mod-php', 'php-cli', 'php-mbstring', 'php-sqlite3', 'php-opcache', 'php-json', 'php-mysql', 'php-pgsql', 'php-ldap', 'php-gd', 'php-xml'] do
+# Install additional PHP extensions required modules for downloading/handling/running Kanboard
+#package %w['libapache2-mod-php', 'php-cli', 'php-mbstring', 'php-sqlite3', 'php-opcache', 'php-json', 'php-mysql', 'php-pgsql', 'php-ldap', 'php-gd', 'php-xml', 'unzip', 'wget'] do
+package %w[libapache2-mod-php php-cli php-mbstring php-sqlite3 php-opcache php-json php-mysql php-pgsql php-ldap php-gd php-xml unzip wget] do
   action :install
 end
 
-# Ensure Apache service is started and enabled
+# Ensure the Apache web service has started and is enabled
 service 'apache2' do
-  action [:start, :enable]
+  action [:enable, :start]
   retries 5
 end
 
-# Restart Apache service to apply changes
+# Download the Kanboard .zip from GitHub to temp folder. This should automatically detect the latest version!
+# Possibly create an environemtal variable that can be updated with the link
+remote_file '/tmp/kanboard-latest.zip' do
+  source 'https://github.com/kanboard/kanboard/archive/master.zip'
+  action :create
+end
+
+# Unzip the downloaded Kanboard file directly to www dir && rename it
+
+execute 'unzip_kanboard' do
+  command 'unzip /tmp/kanboard-latest.zip -d /var/www/ && mv /var/www/kanboard-master /var/www/kanboard'
+  not_if { ::File.exist?('/var/www/kanboard') }
+end
+
+=begin
+execute 'unzip_kanboard' do
+  command 'unzip /tmp/kanboard-latest.zip -d /var/www/'
+  not_if { ::File.exist?('/var/www/kanboard-master') }
+end
+
+execute 'move_kanboard' do
+  command 'mv /var/www/kanboard-mainmaster /var/www/kanboard'
+  not_if { ::File.exist?('/var/www/kanboard') }
+end
+=end
+
+# Change ownership of the Kan dir. www-data is what Apache runs. (Allows Apache to read and write to Kanboard)
+#Should sudo be used here? How would the password auth work?
+execute 'set_permissions' do
+  command 'chown -R www-data:www-data /var/www/kanboard'
+end
+
+# Configure Apache to reach Kanboard
+template '/etc/apache2/sites-available/kanboard.conf' do
+  source 'kanboard.conf.erb'
+  notifies :run, 'execute[enable_kanboard_site]', :immediately
+  notifies :run, 'execute[disable_default_site]', :immediately
+  notifies :restart, 'service[apache2]', :immediately
+end
+
+# Enable the Kanboard site configuration in Apache
+execute 'enable_kanboard_site' do
+  command 'a2ensite kanboard.conf'
+  action :nothing
+end
+
+# Disable the default Apache site configuration
+execute 'disable_default_site' do
+  command 'a2dissite 000-default.conf'
+  action :nothing
+end
+
+# Check Apache status, restart if necessary
+
+=begin
 service 'apache2' do
-  action :restart
+  action [:restart]
+  not_if [ ::service.running?('apache2') ]
+=end
+
+bash 'check_apache_running' do
+  code <<-EOH
+    if ! systemctl is-active --quiet apache2; then
+      systemctl restart apache2
+    fi
+  EOH
+  action :run
+end
+
+# Ensure Apache is enabled and active
+service 'apache2' do
+  action [:enable, :start]
 end
--- appserv_kanboard_cb/test/integration/default/default_test.rb
+++ appserv_kanboard_cb/test/integration/default/default_test.rb
@@ -1,16 +1,33 @@
-# Chef InSpec test for recipe appserv_kanboard_cb::default
-
-# The Chef InSpec reference, with examples and extensive documentation, can be
-# found at https://docs.chef.io/inspec/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
-end
-
+=begin
 # This is an example test, replace it with your own test.
 describe port(80), :skip do
   it { should_not be_listening }
 end
+=end
+
+describe service('apache2') do
+  it { should be_installed}
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('php') do
+  it { should be_installed}
+  it { should be_enabled }
+  it { should be_running }
+end
+
+
+# test/integration/default/kanboard_spec.rb
+describe http('http://localhost') do
+  its('status') { should cmp 200 }
+  its('body') { should match /Kanboard/ }
+end
+
+describe file('/var/www/kanboard/data') do
+  it {should exist}
+  it {should be_directory}
+  its('owner') { should eq 'www-data'}
+  its('group') {should eq 'www-data'}
+end
+
